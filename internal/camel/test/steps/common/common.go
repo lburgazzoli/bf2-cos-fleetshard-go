eef61ac9aef2c4178a24cce036476c528c368afc
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-01-24 15:50:44 +0100

    it

--- internal/camel/test/steps/common/common.go
+++ internal/camel/test/steps/common/common.go
@@ -1,11 +1,16 @@
-package steps
+package common
 
 import (
 	"github.com/cucumber/godog"
+	"github.com/rs/xid"
 	"gitub.com/lburgazzoli/bf2-cos-fleetshard-go/pkg/test/cucumber"
 )
 
 func init() {
 	cucumber.StepModules = append(cucumber.StepModules, func(ctx *godog.ScenarioContext, s *cucumber.TestScenario) {
+		ctx.Step(`^I store an UID as \${([^"]*)}$`, func(as string) error {
+			s.Variables[as] = xid.New().String()
+			return nil
+		})
 	})
 }
---
+++ internal/camel/test/steps/k8s/k8s.go
@@ -0,0 +1,11 @@
+package k8s
+
+import (
+	"github.com/cucumber/godog"
+	"gitub.com/lburgazzoli/bf2-cos-fleetshard-go/pkg/test/cucumber"
+)
+
+func init() {
+	cucumber.StepModules = append(cucumber.StepModules, func(ctx *godog.ScenarioContext, s *cucumber.TestScenario) {
+	})
+}
